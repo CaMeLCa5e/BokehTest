1ef2201861214c44ff79343918a7f2e971324766
Jack Martin <JM273606@gmail.com>
2014-12-28 14:58:49 -0500

    updates

---
+++ bokehPlotting.py
@@ -0,0 +1,40 @@
+from bokeh.plotting import figure, output_file, show
+
+# #prepare data 
+# x = [1,2,3,4,5,5, 8, 90, 12, 44]
+# y = [7,8,4,3,7,8, 9,4,2,33,3,6,8,0]
+x0 = [1,2,3,4,5]
+y1 = [x**2 for x in x0]
+y2 = [10**x for x in x0]
+y3 = [10**(x**2) for x in x0]
+
+#output to static HTML output_file
+output_file("lines.html", title="line plot example")
+
+#plot a 'line' renderer setting the color, line thickness and legend value
+
+p = figure(
+	tools='pan, box_zoom, reset, previewsave'
+    y_axis_type="log", y_range=[0.001, 10**22], title="log axis example",
+	x_axis_label='sections', y_axis_label='particles'
+)
+
+
+#create plots 
+p.line(x0, x0, legend="y=x")
+p.circle(x0, x0, legend="y=x")
+p.line(x0, y1, legend="y=x**2")
+p.circle(x0, y1, fill_color=None, line_color="green", legend="y=x**2")
+p.line(x0, y2, line_color="red", line_width=2, legend="y=10^x")
+p.line(x0, y3, line_color="orange", line_width=2, legend="y=10^(x^2)")
+
+
+show()
+
+
+"""
+notes - saving to lines.html for data storage? does this create lines.1.html for a second run? (file is overwritten)
+
+Why is this importing numpy if np is not called in the script?  
+
+"""
